f5389f13989b191920d1c233161af29949051fd4
jalexxx <jumaanahalex@gmail.com>
2022-02-26 18:25:48 +0000

    made a div for big blockasasded

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 function renderSA_building() {
-  const main = document.querySelector("main");
+  document.createElement("main");
+  const main = document.querySelector("main"); 
   
-
+  document.createElement("div");
   const big_block = document.createElement("div");
   big_block.id = "big_block";
   main.appendChild(big_block);
 
   
-}
-module.exports = {
-  renderSA_building
 };
+
+renderSA_building();
