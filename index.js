8c7cc035f232af89d271581a7723b1062c907d8a
jalexxx <jumaanahalex@gmail.com>
2022-02-26 14:07:48 +0000

    created a function and exported

---
+++ index.js
@@ -0,0 +1,13 @@
+function renderSA_building() {
+  const main = document.querySelector("main");
+  
+
+  const extra_book = document.createElement("div");
+  extra_book.id = "extra_book";
+  main.appendChild(extra_book);
+
+  
+}
+module.exports = {
+  renderSA_building
+};
